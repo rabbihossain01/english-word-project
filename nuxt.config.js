1d7c45470e56c2452dfe8f8c290d179727456a97
nasimuddin01 <debater.nasim@gmail.com>
2021-10-25 20:25:32 +0600

    Global style sheet add

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/styles/style.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [{ src: '~/plugins/fireauth.js', ssr: false }],
