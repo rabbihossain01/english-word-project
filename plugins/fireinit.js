99c62fc2983f0e22fc44686b3b3f2708d921132d
nasim145142 <nasim@bitstrapped.com>
2021-12-01 21:31:37 +0600

    Project credentials remove

--- plugins/fireinit.js
+++ plugins/fireinit.js
@@ -2,14 +2,15 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
+// input your project credentials here
 const firebaseConfig = {
-  apiKey: 'AIzaSyD5FkvNZvnPART6jItRm4Yrh2ofTmS8o6c',
-  authDomain: 'english-batayon.firebaseapp.com',
-  projectId: 'english-batayon',
-  storageBucket: 'english-batayon.appspot.com',
+  apiKey: 'xxxx',
+  authDomain: 'xxx.firebaseapp.com',
+  projectId: 'xx-xx',
+  storageBucket: 'xx-xxx.appspot.com',
   messagingSenderId: '550711462248',
-  appId: '1:550711462248:web:14c8b8222652935b992edd',
-  measurementId: 'G-1KZ821FGT1',
+  appId: '1:xxx:web:14c8b8222652935b992edd',
+  measurementId: 'G-xx',
 }
 
 if (!firebase.apps.length) {
